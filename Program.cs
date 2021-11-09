eb751e3120fc4c1bbff8901d5e8ab693fe25c217
Benjamin Larsen <beela93@hotmail.com>
2021-11-09 12:28:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Server;
+using System;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer server = new();
+            server.Open("127.0.0.1", 8080);
+        }
+    }
+}
